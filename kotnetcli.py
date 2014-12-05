fdc791f8bbbb4bc2f84644547619bb90b91d0d5d
GijsTimmers <timmers.gijs@gmail.com>
2014-12-05 17:12:28 +0100

    extra werk voor travis, hij kan 't heus wel aan

--- kotnetcli.py
+++ kotnetcli.py
@@ -132,10 +132,10 @@ def aanstuurderObvArgumenten(argumenten):
     ############## 1. parse credential-related flags ##############
     cr = Credentials()
     #print argumenten.__dict__
-    if argumenten.worker == "dummy_login"
+    if argumenten.worker == "dummy_login":
         print "ik wil credentials ophalen voor spek en bonen"
         gebruikersnaam, wachtwoord = cr.dummy()
-    elif argumenten.worker == "dummy_logout"
+    elif argumenten.worker == "dummy_logout":
         print "ik wil credentials ophalen voor spek en bonen"
         gebruikersnaam, wachtwoord = cr.dummy()
     else:
