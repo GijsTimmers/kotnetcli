5987ec43bd16c18b8c19adab60fc2776b3b8906f
GijsTimmers <timmers.gijs@gmail.com>
2014-12-05 15:16:23 +0100

    flags are now mutually exclusive

--- kotnetcli.py
+++ kotnetcli.py
@@ -10,7 +10,7 @@
 ##                  http://creativecommons.org/licenses/by-sa/4.0/
 
 ## This work is licensed under the Creative Commons
-## Attribution-ShareAlike 4.0 International License. To view a copy of 
+## Attribution-ShareAlike 4.0 International License. To  view a copy of 
 ## this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
 ## send a letter to Creative Commons, PO Box 1866, Mountain View, 
 ## CA 94042, USA.
@@ -48,130 +48,180 @@ def main(co, gebruikersnaam, wachtwoord, actie="inloggen"):
 def argumentenParser():
     parser = argparse.ArgumentParser(description="Script om in- of uit \
     te loggen op KotNet")
-
-    parser.add_argument("-i", "--login",\
-    help="Logs you in on KotNet (default)",\
-    action="store_true")
-
-    parser.add_argument("-o", "--logout",\
-    help="Logs you out off KotNet",\
-    action="store_true")
-
-    parser.add_argument("-f", "--forget",\
-    help="Makes kotnetcli forget your credentials",\
-    action="store_true")
     
-    parser.add_argument("-g", "--guest-mode",\
+    workergroep = parser.add_mutually_exclusive_group()
+    credentialsgroep = parser.add_mutually_exclusive_group()
+    communicatorgroep = parser.add_mutually_exclusive_group()
+    
+    ## We create three different groups, whose arguments can't be mixed (using
+    ## the add_mutually_exclusive_group() option. If you enter non-combinable
+    ## options, you'll get an error.
+    
+    ## Then, we create three dests: worker, credentials and communicator. 
+    ## The value to each of these dests depends on the flags the user applies.
+    ## If he applies none, each dest will use a default value, set with the
+    ## default parameter in add_argument().
+    
+    ## These two things void the need for complex decision trees.
+    
+    workergroep.add_argument("-i", "--login",\
+    help="Logs you in on KotNet (default)",\
+    action="store_const", dest="worker", const="login", default="login")
+
+    workergroep.add_argument("-o", "--logout",\
+    help="Logs you out off KotNet",\
+    action="store_const", dest="worker", const="logout")
+    
+    workergroep.add_argument("-1", "--dummy-login",\
+    help="Performs a dry-run logging in",\
+    action="store_const", dest="worker", const="dummy_login")
+    
+    workergroep.add_argument("-2", "--dummy-logout",\
+    help="Performs a dry-run logging out",\
+    action="store_const", dest="worker", const="dummy_logout")
+    
+    credentialsgroep.add_argument("-k", "--keyring",\
+    help="Makes kotnetcli pick up your credentials from the keyring (default)",\
+    action="store_const", dest="credentials", const="keyring", \
+    default="keyring")
+
+    credentialsgroep.add_argument("-f", "--forget",\
+    help="Makes kotnetcli forget your credentials",\
+    action="store_const", dest="credentials", const="forget")
+    
+    credentialsgroep.add_argument("-g", "--guest-mode",\
     help="Logs you in as a different user without forgetting your \
     default credentials",\
-    action="store_true")
+    action="store_const", dest="credentials", const="guest_mode")
     
-    parser.add_argument("-c", "--curses",\
+    communicatorgroep.add_argument("-c", "--colortext",\
+    help="Logs you in using colored text output (default)",\
+    action="store_const", dest="communicator", const="colortext", \
+    default="colortext")
+    
+    communicatorgroep.add_argument("-u", "--curses",\
     help="Logs you in using curses output",\
-    action="store_true")
+    action="store_const", dest="communicator", const="curses")
     
-    parser.add_argument("-t", "--plaintext",\
+    communicatorgroep.add_argument("-t", "--plaintext",\
     help="Omits the curses interface by using plaintext output",\
-    action="store_true")
+    action="store_const", dest="communicator", const="plaintext")
     
-    parser.add_argument("-d", "--dialog",\
+    communicatorgroep.add_argument("-d", "--dialog",\
     help="Omits the curses inteface by using dialog based output",\
-    action="store_true")
+    action="store_const", dest="communicator", const="dialog")
     
-    parser.add_argument("-b", "--bubble",\
+    communicatorgroep.add_argument("-b", "--bubble",\
     help="Hides all output except for a bubble notification",\
-    action="store_true")
+    action="store_const", dest="communicator", const="bubble")
     
-    parser.add_argument("-s", "--summary",\
+    communicatorgroep.add_argument("-s", "--summary",\
     help="Hides all output except for a small summary",\
-    action="store_true")
+    action="store_const", dest="communicator", const="summary")
 
-    parser.add_argument("-q", "--quiet",\
+    communicatorgroep.add_argument("-q", "--quiet",\
     help="Hides all output",\
-    action="store_true")
+    action="store_const", dest="communicator", const="quiet")
     
-    parser.add_argument("-1", "--dummy-login",\
-    help="Performs a dry-run logging in",\
-    action="store_true")
-    
-    parser.add_argument("-2", "--dummy-logout",\
-    help="Performs a dry-run logging out",\
-    action="store_true")
-
     argumenten = parser.parse_args()
-    return argumenten
-
-def aanstuurderObvArgumenten(argumenten):
+    #print argumenten.__dict__
     
+    """
+    if \
+    argumenten.keyring      == False and \
+    argumenten.forget       == False and \
+    argumenten.guest_mode   == False:
+        argumenten.keyring   = True
+    
+    if \
+    argumenten.curses       == False and \
+    argumenten.colortext    == False and \
+    argumenten.plaintext    == False and \
+    argumenten.dialog       == False and \
+    argumenten.bubble       == False and \
+    argumenten.summary      == False and \
+    argumenten.quiet        == False:
+        argumenten.colortext = True
+    
+    if \
+    argumenten.login        == False and \
+    argumenten.logout       == False and \
+    argumenten.dummy_login  == False and \
+    argumenten.dummy_logout == False:
+        argumenten.login     = True
+    
+    """
+    return(argumenten)
+    
+def aanstuurderObvArgumenten(argumenten):    
     ############## 1. parse credential-related flags ##############
     cr = Credentials()
-    if argumenten.forget:
-        print "ik wil vergeten"
-        cr.forget()
-        return()
-    
-    if argumenten.guest_mode:
-        print "ik wil me anders voordoen dan ik ben"
-        gebruikersnaam, wachtwoord = cr.guest()
-    else:
+    #print argumenten.__dict__
+    if argumenten.credentials == "keyring":
         print "ik haal de credentials uit de keyring"
         gebruikersnaam, wachtwoord = cr.getset()
+    
+    elif argumenten.credentials == "forget":
+        print "ik wil vergeten"
+        cr.forget()
+    
+    elif argumenten.credentials == "guest_mode":
+        print "ik wil me anders voordoen dan ik ben"
+        gebruikersnaam, wachtwoord = cr.guest()
         
     ############## 2. switch on communicator-related flags ##############
-        
-    if argumenten.curses:
+    if argumenten.communicator == "curses":
         print "ik wil vloeken"
         if os.name == "posix":
             co = communicator.CursesCommunicator()
         else:
             co = communicator.ColoramaCommunicator()
-                
-    elif argumenten.plaintext:
+    
+    elif argumenten.communicator == "colortext":
+        print "ik wil vrolijke kleuren"
+        co = communicator.ColoramaCommunicator()
+    
+    elif argumenten.communicator == "plaintext":
         print "ik wil terug naar de basis"
         co = communicator.PlaintextCommunicator()
     
-    elif argumenten.dialog:
+    elif argumenten.communicator == "dialog":
         print "ik wil fancy dialogs"
-        co = communicator.DialogCommunicator()
-    
-    elif argumenten.bubble:
+        if os.name == "posix":
+            co = communicator.DialogCommunicator()
+        else:
+            co = communicator.ColoramaCommunicator()
+        
+    elif argumenten.communicator == "bubble":
         print "ik wil bellen blazen"
-        co = communicator.BubbleCommunicator()
-    
-    elif argumenten.summary:
+        if os.name == "posix":
+            co = communicator.BubbleCommunicator()
+        else:
+            co = communicator.ColoramaCommunicator()
+        
+    elif argumenten.communicator == "summary":
         print "ik wil het mooie in de kleine dingen zien"
         co = communicator.SummaryCommunicator()
         
-    elif argumenten.quiet:
+    elif argumenten.communicator == "quiet":
         print "ik wil zwijgen"
         co = communicator.QuietCommunicator()
-    else:
-        ## no communicator specified; choose default communicator
-        print "ik wil wat kleur in mijn leven aanbrengen"
-        co = communicator.ColoramaCommunicator()
-        
     
     ############## 3. switch on login-type flags ##############
+    if argumenten.worker == "login":
+        print "ik wil inloggen"
+        main(co, gebruikersnaam, wachtwoord, actie="inloggen")
     
-    if argumenten.dummy_login:
-        print "ik wil inloggen voor spek en bonen"
-        main(co, gebruikersnaam, wachtwoord, actie="dummyinloggen")
-    
-    elif argumenten.dummy_logout:
-        print "ik wil uitloggen voor spek en bonen"
-        main(co, gebruikersnaam, wachtwoord, actie="dummyuitloggen")
-    
-    elif argumenten.logout:
+    elif argumenten.worker == "logout":
         print "ik wil uitloggen"
         main(co, gebruikersnaam, wachtwoord, actie="uitloggen")
     
-    elif argumenten.login:    
-        print "ik wil inloggen met een --login vlag"
-        main(co, gebruikersnaam, wachtwoord, actie="inloggen")
+    elif argumenten.worker == "dummy_login":
+        print "ik wil inloggen voor spek en bonen"
+        main(co, gebruikersnaam, wachtwoord, actie="dummyinloggen")
     
-    else:
-        print "ik wil inloggen zonder vlag"
-        main(co, gebruikersnaam, wachtwoord, actie="inloggen")
-
+    elif argumenten.worker == "dummy_logout":
+        print "ik wil uitloggen voor spek en bonen"
+        main(co, gebruikersnaam, wachtwoord, actie="dummyuitloggen")
 
 aanstuurderObvArgumenten(argumentenParser())
