b7c4b559d40ce1fd35e702bf45cc664682ac05a2
GijsTimmers <timmers.gijs@gmail.com>
2015-01-07 00:10:14 +0100

    created compilation directory

---
+++ compilation/compile-lin.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+if [ -d "pyinstaller" ]; then
+    echo "OK, pyinstaller aanwezig..."
+#fi
+
+#if [ ! -d "pyinstaller" ]; then
+else
+    echo "pyinstaller afwezig, gaat nu clonen..."
+    git clone https://github.com/pyinstaller/pyinstaller.git
+fi
+
+./pyinstaller/pyinstaller.py compile-lin.spec &&\
+
+echo "Compileren succesvol."
+
+
+
