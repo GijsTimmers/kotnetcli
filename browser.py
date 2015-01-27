bece564f6eea4e72ecde230c96a6e000fbda3b7b
GijsTimmers <timmers.gijs@gmail.com>
2015-01-28 00:36:03 +0100

    Splitted worker; improved form handling

---
+++ browser.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+## Dependencies:    python-mechanize, python-keyring, curses
+## Author:          Gijs Timmers: https://github.com/GijsTimmers
+## Contributors:    Gijs Timmers: https://github.com/GijsTimmers
+##                  Jo Van Bulck: https://github.com/jovanbulck
+
+## Licence:         CC-BY-SA-4.0
+##                  https://creativecommons.org/licenses/by-sa/4.0/
+
+## This work is licensed under the Creative Commons
+## Attribution-ShareAlike 4.0 International License. To view a copy of
+## this license, visit https://creativecommons.org/licenses/by-sa/4.0/ or
+## send a letter to Creative Commons, PO Box 1866, Mountain View,
+## CA 94042, USA.
+
+import re                               ## Basislib voor reguliere expressies
+import time                             ## Voor timeout om venster te sluiten
+import urllib                           ## Diverse URL-manipulaties
+import urlparse                         ## Diverse URL-manipulaties
+import mechanize                        ## Emuleert een browser
+import socket                           ## Voor ophalen IP
+import sys                              ## Basislib
+import os                               ## Basislib
+
+from bs4 import BeautifulSoup, Comment  ## Om webinhoud proper te parsen.
+
+## The class doing the actual browser emulation work. One can extend this
+## class to add specific behavior (e.g. communicating; err catching; etc).
+## KotnetBrowser() is like an API: it contains all possible Browser operations.
+## So, we don't create a LoginBrowser(), LogoutBrowser(), etc. Instead, the
+## proper Worker() is instantiated by kotnetcli.py, and this instance calls
+## only the Browser() methods that it needs.
+## Some 
+
+class KotnetBrowser():
+    def __init__(self, gebruikersnaam, wachtwoord):
+        self.browser = mechanize.Browser()
+        self.browser.addheaders = [('User-agent', 'Firefox')]
+        self.gebruikersnaam = gebruikersnaam    #TODO niet opslaan...
+        self.wachtwoord = wachtwoord
+        
+    def login_open_netlogin(self):
+        response = self.browser.open("https://netlogin.kuleuven.be", \
+        timeout=1.8)
+        #html = response.read()
+
+    def login_kies_kuleuven(self):
+        self.browser.select_form(nr=1)
+        self.browser.submit()
+    
+    def login_input_credentials(self):
+        self.browser.select_form(nr=1)
+        self.browser.form["uid"] = self.gebruikersnaam
+        wachtwoordvaknaam = \
+        self.browser.form.find_control(type="password").name
+        self.browser.form[wachtwoordvaknaam] = self.wachtwoord
+
+    def login_send_credentials(self):
+        self.browser.submit()
+
+    ## This method parses the server's response. On success, it returns a tuple of
+    ## length 2: (downloadpercentage, uploadpercentage); else this method
+    ## returns a tuple of length 1, containing a descriptive errmsg
+    def login_parse_results(self):
+        html = self.browser.response().read()
+
+        soup = BeautifulSoup(html)
+
+        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
+        ## bevat de status.
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        p = re.compile("weblogin: rc=\d+")
+        for c in comments:
+            m = p.search(c)
+            #m = p.findall(c)
+            #print m
+            if m:
+                rccode = int(m.group().strip("weblogin: rc="))
+                ## m = p.search(c): zoekt naar de RE p in c. Indien die is
+                ## gevonden, is m een object; m.group() geeft vervolgens
+                ## weer waar de RE matcht. Dit doet het maar één keer; als
+                ## je strings op meerdere matches wilt controleren, moet je
+                ## p.findall(c) gebruiken, zoals hieronder.
+
+        if rccode == 100:
+            ## succesvolle login
+            ## downloadpercentage parsen
+            p = re.compile("\d+")
+            m = p.findall(comments[6])
+            downloadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
+
+            ## uploadpercentage parsen
+            p = re.compile("\d+")
+            m = p.findall(comments[7])
+            uploadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
+
+            return (downloadpercentage, uploadpercentage)
+
+        elif rccode == 202:
+            ## verkeerd wachtwoord
+            return("Uw logingegevens kloppen niet. Gebruik kotnetcli " + \
+            "--forget om deze te resetten.",)
+
+        elif rccode == 206:
+            ## al ingelogd op ander IP
+            return ("U bent al ingelogd op een ander IP-adres. Gebruik " + \
+            "kotnetcli --force-login om u toch in te loggen.",)
+
+        else:
+            print html
+            return ("\nrc-code onbekend. Stuur bovenstaande informatie naar \
+            gijs.timmers@student.kuleuven.be om ondersteuning te krijgen.",)
+    
+    def logout_input_credentials(self):
+        ## Lokale IP ophalen: lelijk, maar werkt goed en is snel. Is waar-
+        ## schijnlijk de kortste code die crossplatform werkt, wat op zich
+        ## wel vreemd is. Meer informatie:        
+        ## http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com", 80))
+        self.uitteloggenip = s.getsockname()[0]
+        s.close()
+        
+        ## Logoutformulier aanmaken, dan lokatie omvormen tot file://-URL zodat
+        ## het browserobject hem kan gebruiken als lokatie.
+        gegevens = {"uitteloggenip" : self.uitteloggenip, 
+                    "gebruikersnaam": self.gebruikersnaam}
+        
+        with open("logoutformuliertemplate.html", "r") as logoutformulier_in:
+            data = logoutformulier_in.read()
+            data = data.format(**gegevens) ## gegevens worden hier ingevuld
+        with open("logoutformulieringevuld.html", "w") as logoutformulier_out:
+            logoutformulier_out.write(data)
+        
+        self.url_logoutformulier = urlparse.urljoin("file:", \
+        os.path.abspath("logoutformulieringevuld.html"))
+    
+    def logout_send_credentials(self):
+        self.browser.open(self.url_logoutformulier, timeout=1.8)
+        self.browser.select_form(nr=0)
+        self.browser.submit()
+        os.remove("logoutformulieringevuld.html")
+    
+    ## returns True if logout is succesful; False when it fails
+    def logout_parse_results(self):
+        html = self.browser.response().read()
+        soup = BeautifulSoup(html)
+
+        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
+        ## bevat de status.
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        p = re.compile("weblogout: rc=\d+")
+
+        rccode = 100
+        ## if not error codes appear, assume that everything went OK.
+        for c in comments:
+            m = p.search(c)
+            #m = p.findall(c)
+            #print m
+            if m:
+                rccode = int(m.group().strip("weblogout: rc="))
+
+        if rccode == 100:
+            ## succesvolle logout
+            return True
+
+        elif rccode == 207:
+            ## al uitgelogd
+            print "U had uzelf reeds succesvol uitgelogd."
+            return False
+        else:
+            print html
+    
+## Deze klasse bevat functies voor de forceer-loginmethode. Gaan we nu nog
+## niet gebruiken. Deze klasse wordt later waarschijnlijk samengevoegd met
+## KotnetBrowser().
+class KotnetForceerBrowser(KotnetBrowser):
+    def __init__():
+        pass
+    
+    def uitteloggenipophalen(self):
+        html = self.browser.response().read()
+        soup = BeautifulSoup(html)
+
+        forms = soup.findAll("form")
+        form = forms[1]
+        uitteloggenip = form.contents[3]["value"]
+
+        return uitteloggenip    
+
+class DummyBrowser():
+    def __init__(self, gebruikersnaam, wachtwoord):
+        pass
+
+    def open_netlogin(self):
+        time.sleep(0.1)
+
+    def netlogout(self, lokatie_formulier):
+        time.sleep(0.1)
+
+    def kies_kuleuven(self):
+        time.sleep(0.1)
+
+    def input_credentials(self):
+        time.sleep(0.1)
+
+    def send_credentials(self):
+        time.sleep(0.1)
+
+    ## This method parses the server's response. On success, it returns a tuple of
+    ##  length 2: (downloadpercentage, uploadpercentage); else this method
+    ##  returns a tuple of length 1, containing a descriptive errmsg
+    def parse_percentages(self):
+        return (80, 100)
+
+    def uitteloggenipophalen(self):
+        return "192.168.10.10"
+
+    ## returns true iff succes; else false
+    def parse_logout(self):
+        return True
--- worker.py
+++ worker.py
@@ -26,6 +26,7 @@
 import re                               ## Basislib voor reguliere expressies
 import time                             ## Voor timeout om venster te sluiten
 import urllib                           ## Diverse URL-manipulaties
+import browser                          ## Doet het eigenlijke browserwerk
 import urlparse                         ## Diverse URL-manipulaties
 import mechanize                        ## Emuleert een browser
 import socket                           ## Voor ophalen IP
@@ -36,385 +37,169 @@ from tools import pinger                ## Om te checken of we op Kotnet zitten
 from tools import errorcodes as error   ## Om magic number errors te voorkomen
 from bs4 import BeautifulSoup, Comment  ## Om webinhoud proper te parsen.
 
+EXIT_FAILURE = 1 ## Tijdelijke exitcode, moet nog worden geïmplementeerd.
 
-
-class Kotnetlogin():
-    def __init__(self, co, gebruikersnaam, wachtwoord, afsluiten=True):
-        
-        self.browser = mechanize.Browser()
-        self.browser.addheaders = [('User-agent', 'Firefox')]
-        
-        self.gebruikersnaam = gebruikersnaam
-        self.wachtwoord = wachtwoord
-        
+class SuperWorker():
+    def __init__(self, co, gebruikersnaam, wachtwoord):
         self.co = co
-        self.afsluiten = afsluiten
+        self.browser = browser.KotnetBrowser(gebruikersnaam, wachtwoord)
+    
+    """ Reden voor uitcommenten: Browser() gebruikt geen abstracte klasses. 
+    Daardoor is login_input_credentials() heel anders dan logout_input_creden-
+    tials en kunnen we niet input_credentials() aanroepen.
+    
+    def go(self):
+        self.netlogin()
+        self.kuleuven()
+        self.gegevensinvoeren()
+        self.gegevensopsturen()
+        self.tegoeden()
+
+    def netlogin(self):
+            pass
+
+    def kuleuven(self):
+            pass
+
+    def gegevensinvoeren(self):
+        self.co.eventInvoerenStart()
+        try:
+            self.browser.input_credentials()
+            self.co.eventInvoerenSuccess()
+        except:
+            self.co.eventInvoerenFailure()
+            sys.exit(EXIT_FAILURE)
+
+    def gegevensopsturen(self):
+        self.co.eventOpsturenStart()
+        try:
+            self.browser.send_credentials()
+            self.co.eventOpsturenSuccess()
+        except:
+            self.co.eventOpsturenFailure()
+            sys.exit(EXIT_FAILURE)
+
+    def tegoeden(self):
+            pass
+    """
+
+## A worker class that either succesfull logs you in to kotnet
+## or exits with failure, reporting events to the given communicator
+class Kotnetlogin(SuperWorker):
+    def go(self):
+        self.netlogin()
+        self.kuleuven()
+        self.gegevensinvoeren()
+        self.gegevensopsturen()
+        self.resultaten()
         
     def netlogin(self):
         self.co.eventNetloginStart()
         try:
-            respons = self.browser.open("https://netlogin.kuleuven.be", \
-            timeout=1.8)
-            html = respons.read()
+            self.browser.login_open_netlogin()
             self.co.eventNetloginSuccess()
         except:
             self.co.eventNetloginFailure()
-            #sys.exit(1)
-            sys.exit(error.NOHOST)
-            ## voorbeeld van hoe het er uit moet komen zien
-        
+            sys.exit(EXIT_FAILURE)
+
     def kuleuven(self):
         self.co.eventKuleuvenStart()
         try:
-            self.browser.select_form(nr=1)
-            self.browser.submit()
+            self.browser.login_kies_kuleuven()
             self.co.eventKuleuvenSuccess()
         except:
             self.co.eventKuleuvenFailure()
-            sys.exit(1)
-
+            sys.exit(EXIT_FAILURE)
+    
     def gegevensinvoeren(self):
         self.co.eventInvoerenStart()
         try:
-            self.browser.select_form(nr=1)
-            self.browser.form["uid"] = self.gebruikersnaam
-            wachtwoordvaknaam = \
-            self.browser.form.find_control(type="password").name
-            
-            self.browser.form[wachtwoordvaknaam] = self.wachtwoord
+            self.browser.login_input_credentials()
             self.co.eventInvoerenSuccess()
         except:
             self.co.eventInvoerenFailure()
-            sys.exit(1)
-        
-        
+            sys.exit(EXIT_FAILURE)
+
     def gegevensopsturen(self):
         self.co.eventOpsturenStart()
         try:
-            self.browser.submit()
+            self.browser.login_send_credentials()
             self.co.eventOpsturenSuccess()
         except:
             self.co.eventOpsturenFailure()
-            sys.exit(1)
-        
-        
-    def tegoeden(self):
-        html = self.browser.response().read()
-        
-        soup = BeautifulSoup(html)
-        
-        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
-        ## bevat de status.
-        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-        p = re.compile("weblogin: rc=\d+")
-        for c in comments:
-            m = p.search(c)
-            #m = p.findall(c)
-            #print m
-            if m:
-                rccode = int(m.group().strip("weblogin: rc="))
-                ## m = p.search(c): zoekt naar de RE p in c. Indien die is
-                ## gevonden, is m een object; m.group() geeft vervolgens 
-                ## weer waar de RE matcht. Dit doet het maar één keer; als
-                ## je strings op meerdere matches wilt controleren, moet je
-                ## p.findall(c) gebruiken, zoals hieronder.
-        
-        if rccode == 100:            
-            ## succesvolle login
-            ## downloadpercentage parsen
-            p = re.compile("\d+")
-            m = p.findall(comments[6])
-            self.downloadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
-            
-            ## uploadpercentage parsen
-            p = re.compile("\d+")
-            m = p.findall(comments[7])
-            self.uploadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
-            
-            
-            self.co.eventLoginGeslaagd(self.downloadpercentage, \
-            self.uploadpercentage)
-            
-            #self.co.eventSamenvattingGeven("login", True)
-            self.co.beeindig_sessie(0)
-            return(True)
-            
-        elif rccode == 202:
-            ## verkeerd wachtwoord
-            print "Uw logingegevens kloppen niet. Gebruik kotnetcli " + \
-            "--forget om deze te resetten."
-            self.co.beeindig_sessie(1)
-            return(True)
-        
-        elif rccode == 206:
-            ## al ingelogd op ander IP
-            print "U bent al ingelogd op een ander IP-adres. Gebruik " + \
-            "kotnetcli --force-login om u toch in te loggen." 
-            
-            self.co.beeindig_sessie(1)
-            return(False)
-        
+            sys.exit(EXIT_FAILURE)
+
+    def resultaten(self):
+        tup = self.browser.login_parse_results()
+        ## check whether it worked out
+        if len(tup) == 1:
+            print tup[0]
         else:
-            self.co.beeindig_sessie(1)
-            print html
-            print "\nrc-code onbekend. Stuur bovenstaande informatie naar"
-            print "gijs.timmers@student.kuleuven.be om ondersteuning te krijgen."
-            
-            return(True)
-            
-        #print self.afsluiten
-        if self.afsluiten:
-            self.co.beeindig_sessie()
-        
-    def uitteloggenipophalen(self):
-        html = self.browser.response().read()
-        soup = BeautifulSoup(html)
-        
-        forms = soup.findAll("form")
-        form = forms[1]
-        uitteloggenip = form.contents[3]["value"]
-        
-        return uitteloggenip
+            self.co.eventResultatenBekend(tup[0], tup[1])
+        self.co.beeindig_sessie()
 
-class Kotnetloguit():
-    def __init__(self, co, gebruikersnaam, wachtwoord, uitteloggenip=None, afsluiten=True):
-        
-        self.browser = mechanize.Browser()
-        self.browser.addheaders = [('User-agent', 'Firefox')]
-        
-        self.gebruikersnaam = gebruikersnaam
-        self.wachtwoord = wachtwoord
-        
+class Dummylogin(Kotnetlogin):
+    def __init__(self, co, gebruikersnaam, wachtwoord):
         self.co = co
-        self.uitteloggenip = uitteloggenip
-        self.afsluiten = afsluiten
-        
-        if self.uitteloggenip == None:
-            ## Lokale IP ophalen: lelijk, maar werkt.
-            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            s.connect(("gmail.com",80))
-            self.uitteloggenip = s.getsockname()[0]
-            s.close()
-        
-        ## Formulier aanmaken. Het opsturen van dit formulier geeft toegang
-        ## tot de noodzakelijke loguitpagina. 
-        bestand = open("formulier.html", "w")
-        bestand.write("<html><body>\n")
-        bestand.write('<FORM METHOD=POST ACTION=' \
-        '"https://netlogin.kuleuven.be/cgi-bin/wayf2.pl">\n')
-        bestand.write('<INPUT type=hidden name="inout" value="logout">\n')
-        bestand.write('<INPUT type=hidden name="ip" value="%s">\n' \
-        % self.uitteloggenip)
-        bestand.write('<INPUT type=hidden name="network" value="KotNet">\n')
-        bestand.write('<INPUT type=hidden name="uid" value="kuleuven/%s">\n' % \
-        self.gebruikersnaam)
-        bestand.write('<INPUT type=hidden name="lang" value="ned">\n')
-        #bestand.write('<INPUT type=submit value="logout">\n')
-        bestand.write('<INPUT type=submit value="logout kuleuven/%s@%s">\n' % (self.gebruikersnaam, self.uitteloggenip))
-        bestand.write('</FORM>\n')
-        bestand.write('</body></html>\n')
-        bestand.close()
-        
-        self.lokatie_formulier = urlparse.urljoin("file:", \
-        urllib.pathname2url(os.path.join(os.getcwd(), "formulier.html")))
+        self.browser = browser.DummyBrowser(gebruikersnaam, wachtwoord);
+
+## A worker class that either succesfull logs you off from kotnet
+## or exits with failure, reporting events to the given communicator
+## Do not use Kotnetlogout for integration in a forced-login method. We have
+## KotnetForceLogin() for this purpose.
+class Kotnetlogout(SuperWorker):
+    def go(self):
+        self.formulieropsturen()
+        self.resultaten()
     
-    def netlogin(self):
-        self.co.eventNetloginStart()
+    def formulieraanmaken(self):
+        self.co.eventFormulierAanmakenStart()
         try:
-            self.browser.open(self.lokatie_formulier, timeout=1.8)                        
-            self.browser.select_form(nr=0)
-            self.browser.submit()
-            os.remove(os.path.join(os.getcwd(), "formulier.html"))
-            self.co.eventNetloginSuccess()
+            self.browser.logout_input_credentials()
+            self.co.eventFormulierAanmakenSuccess()
         except:
-            self.co.eventNetloginFailure()
-            sys.exit(1)
+            self.co.eventFormulierAanmakenFailure()
+            sys.exit(EXIT_FAILURE)
     
-    def kuleuven(self):
-        pass
-    
-    def gegevensinvoeren(self):
-        self.co.eventInvoerenStart()
+    def formulieropsturen(self):
+        self.co.eventFormulierOpsturenStart()
         try:
-            self.browser.select_form(nr=1)
-            wachtwoordvaknaam = \
-            self.browser.form.find_control(type="password").name
-            self.browser.form[wachtwoordvaknaam] = self.wachtwoord
-            self.co.eventInvoerenSuccess()
+            self.browser.logout_send_credentials()
+            self.co.eventFormulierOpsturenSuccess()
         except:
-            self.co.eventInvoerenFailure()
-            sys.exit(1)
-    
-    def gegevensopsturen(self):
-        self.co.eventOpsturenStart()
-        try:
-            self.browser.submit()
-            self.co.eventOpsturenSuccess()
-        except:
-            self.co.eventOpsturenFailure()
-            sys.exit(1)
-    
-    def tegoeden(self):
-        html = self.browser.response().read()
-        #print html
-        soup = BeautifulSoup(html)
-        
-        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
-        ## bevat de status.
-        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-        p = re.compile("weblogout: rc=\d+")
-        
-        rccode = 100 
-        ## if no error codes appear, assume that everything went OK.
-        for c in comments:
-            m = p.search(c)
-            #m = p.findall(c)
-            #print m
-            if m:
-                rccode = int(m.group().strip("weblogout: rc="))
-            
-        if rccode == 100:
-            ## succesvolle logout
-            #self.co.eventSamenvattingGeven("loguit", True)
-            self.co.beeindig_sessie(0)
-            
-        elif rccode == 207:
-            ## al uitgelogd
-            print "U had uzelf reeds succesvol uitgelogd."
-            self.co.beeindig_sessie(0)
-            
+            self.co.eventFormulierOpsturenFailure()
+            sys.exit(EXIT_FAILURE)
+
+    def resultaten(self):
+        if logout_parse_results():
+            print "success!"
         else:
-            self.co.beeindig_sessie(1)
-            print html
-        
-        
-        #print self.afsluiten
-        if self.afsluiten:
-            self.co.beeindig_sessie()
-            ## this should be removed
-        
-class Dummylogin():
-    def __init__(self, co, gebruikersnaam, wachtwoord):
-        
-        self.browser = mechanize.Browser()
-        self.browser.addheaders = [('User-agent', 'Firefox')]
-        
-        self.gebruikersnaam = gebruikersnaam
-        self.wachtwoord = wachtwoord
-        
-        self.co = co
-        
-    def netlogin(self):
-        self.co.eventNetloginStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventNetloginSuccess()
-        except:
-            self.co.eventNetloginFailure()
-            sys.exit(1)
-        
-    def kuleuven(self):
-        self.co.eventKuleuvenStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventKuleuvenSuccess()
-        except:
-            self.co.eventKuleuvenFailure()
-            sys.exit(1)
+            print "outch!"
+        self.co.beeindig_sessie()
 
-    def gegevensinvoeren(self):
-        self.co.eventInvoerenStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventInvoerenSuccess()
-        except:
-            self.co.eventInvoerenFailure()
-            sys.exit(1)
-        
-        
-    def gegevensopsturen(self):
-        self.co.eventOpsturenStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventOpsturenSuccess()
-        except:
-            self.co.eventOpsturenFailure()
-            sys.exit(1)
-        
-        
-    def tegoeden(self):
-        self.downloadpercentage = 10
-        self.uploadpercentage   = 59
-        
-        self.co.eventLoginGeslaagd(self.downloadpercentage, \
-        self.uploadpercentage)
-        
-        self.co.beeindig_sessie(0)
-
-class Dummyloguit():
+class Dummylogout(Kotnetlogout):
     def __init__(self, co, gebruikersnaam, wachtwoord):
-        
-        self.browser = mechanize.Browser()
-        self.browser.addheaders = [('User-agent', 'Firefox')]
-        
-        self.gebruikersnaam = gebruikersnaam
-        self.wachtwoord = wachtwoord
-        
-        self.co = co
-        
-        self.lokaleip = "192.168.1.1"
-        
-        ## Formulier aanmaken. Het opsturen van dit formulier geeft toegang
-        ## tot de noodzakelijke loguitpagina. 
-        bestand = open("formulier.html", "w")
-        bestand.write("<html><body>\n")
-        bestand.write('<FORM METHOD=POST ACTION=' \
-        '"https://netlogin.kuleuven.be/cgi-bin/wayf2.pl">\n')
-        bestand.write('<INPUT type=hidden name="inout" value="logout">\n')
-        bestand.write('<INPUT type=hidden name="ip" value="%s">\n' \
-        % self.lokaleip)
-        bestand.write('<INPUT type=hidden name="network" value="KotNet">\n')
-        bestand.write('<INPUT type=hidden name="uid" value="kuleuven/%s">\n' % \
-        self.gebruikersnaam)
-        bestand.write('<INPUT type=hidden name="lang" value="ned">\n')
-        bestand.write('<INPUT type=submit value="logout">\n')
-        bestand.write('</FORM>\n')
-        bestand.write('</body></html>\n')
-        bestand.close()
-        
-        self.lokatie_formulier = urlparse.urljoin("file:", \
-        urllib.pathname2url(os.path.join(os.getcwd(), "formulier.html")))
-    
-    def netlogin(self):
-        self.co.eventNetloginStart()
-        try:
-            time.sleep(0.1)
-            os.remove(os.path.join(os.getcwd(), "formulier.html"))
-            self.co.eventNetloginSuccess()
-        except:
-            self.co.eventNetloginFailure()
-            sys.exit(1)
-    
-    def kuleuven(self):
-        pass
-    
-    def gegevensinvoeren(self):
-        self.co.eventInvoerenStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventInvoerenSuccess()
-        except:
-            self.co.eventInvoerenFailure()
-            sys.exit(1)
-    
-    def gegevensopsturen(self):
-        self.co.eventOpsturenStart()
-        try:
-            time.sleep(0.1)
-            self.co.eventOpsturenSuccess()
-        except:
-            self.co.eventOpsturenFailure()
-            sys.exit(1)
-    
-    def tegoeden(self):
-        self.co.beeindig_sessie(0)
+        Kotnetlogout.__init__(self, co, gebruikersnaam, wachtwoord)
+        self.browser = browser.DummyBrowser(gebruikersnaam, wachtwoord);
+
+class KotnetForceer(Kotnetlogin):
+
+    def __init__(self, co, gebruikersnaam, wachtwoord):
+        self.my_super_kl = Kotnetlogin.__init__(self, co, gebruikersnaam, wachtwoord)
+
+    def go(self):
+        ## IP van uit te loggen apparaat opzoeken
+        self.netlogin()
+        self.kuleuven()
+        self.gegevensinvoeren()
+        self.gegevensopsturen()
+        self.browser.parse_percentages()
+        uitteloggenip = self.browser.uitteloggenipophalen()
+        print uitteloggenip    #TODO communicator
+
+        ## Ander apparaat uitloggen
+        kl = worker.Kotnetloguit(co, gebruikersnaam, wachtwoord, uitteloggenip=uitteloggenip)
+        kl.go()
+
+        ## Conventionele login
+        self.my_super_kl.go()
