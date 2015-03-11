17a3767e47ab6ba81e71402ca9abe3f2c69fb981
GijsTimmers <timmers.gijs@gmail.com>
2015-03-11 16:14:35 +0100

    App GUI improvements. See comment.
    I've improved the GUI. You can now see what the login should look like by running juiste_verbindingen.py. Click on 'Inloggen'.

---
+++ app-development/juiste_verbindingen.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+## Dependencies:    python-mechanize, python-keyring, curses
+## Author:          Gijs Timmers: https://github.com/GijsTimmers
+## Contributors:    Gijs Timmers: https://github.com/GijsTimmers
+##                  Jo Van Bulck: https://github.com/jovanbulck
+
+## Licence:         CC-BY-SA-4.0
+##                  https://creativecommons.org/licenses/by-sa/4.0/
+
+## This work is licensed under the Creative Commons
+## Attribution-ShareAlike 4.0 International License. To view a copy of 
+## this license, visit https://creativecommons.org/licenses/by-sa/4.0/ or
+## send a letter to Creative Commons, PO Box 1866, Mountain View, 
+## CA 94042, USA.
+
+import re
+import sys
+import time                 ## Enkel om login te simuleren.
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.clock import Clock ## Om een interval in te bouwen, zonder de loop te blokkeren
+from kivy.storage.jsonstore import JsonStore
+
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        id: hoofdbox
+        orientation: "vertical"
+        
+        BoxLayout:
+            id: titelbox
+            orientation: "vertical"
+            size_hint_y: 0.15
+            
+            
+            Label:
+                text: "kotnetcli"
+                font_size: 30
+                size_hint_y: 0.6
+        
+            Label:
+                text: "by Gijs Timmers and Jo Van Bulck"
+                font_size: 14
+                size_hint_y: 0.4
+            
+        BoxLayout:
+            id: knoppenbox
+            orientation: "vertical"
+            
+            Button:
+                id: inlogknop
+                text: "Inloggen"
+                on_press:
+                    app.ga_naar_actiescherm_en_log_in()
+                
+            Button:
+                id: uitlogknop
+                text: "Uitloggen"
+                on_press:
+                    root.manager.current = "action"
+                    root.uitloggen()
+            
+            Button:
+                id: instellingenknop
+                text: "Instellingen"
+                on_press: root.manager.current = "settings"
+
+<ActionScreen>:
+    BoxLayout:
+        id: hoofdbox
+        orientation: "vertical"
+        
+        BoxLayout:
+            id: titelbox
+            orientation: "vertical"
+            size_hint_y: 0.15
+            
+            
+            Label:
+                text: "kotnetcli"
+                font_size: 30
+                size_hint_y: 0.6
+        
+            Label:
+                text: "by Gijs Timmers and Jo Van Bulck"
+                font_size: 14
+                size_hint_y: 0.4
+        
+        
+        BoxLayout:
+            id: knoppenbox
+            orientation: "vertical"
+            
+            CodeInput:
+                id: outputvak
+                font_size: 20
+                size_hint_y: 0.8
+            
+            Button:
+                id: afsluitknop
+                text: "Terug naar hoofdmenu"
+                size_hint_y: 0.2
+                on_press: root.manager.current = 'menu'
+                
+                
+            
+        
+<SettingsScreen>:
+    BoxLayout:
+        orientation: "vertical"
+        
+        Label:
+            text: "kotnetcli"
+            font_size: 30
+            size_hint_y: None
+            height: 60
+        
+        Label:
+            text: "by Gijs Timmers and Jo Van Bulck"
+            font_size: 14
+            size_hint_y: None
+            height: 30
+        
+        TextInput:
+            id: veld_gebruikersnaam
+            hint_text: "Gebruikersnaam"
+            multiline: False
+            size_hint_y: None
+            height: 30
+            keyboard_suggestions: False
+            password: False
+            write_tab: False
+            #on_text_validate: root.stelGebruikersnaamIn(self.text)
+        
+        TextInput:
+            id: veld_wachtwoord
+            hint_text: "Wachtwoord"
+            multiline: False
+            size_hint_y: None
+            height: 30
+            keyboard_suggestions: False
+            password: True
+            write_tab: False
+            #on_text_validate: root.stelWachtwoordIn(self.text)
+        
+        
+        Button:
+            text: "Opslaan"
+            size_hint_y: 0.3
+            on_press: root.toonData()
+        
+        Label:
+            id: toonvak
+            text: "Nog geen data ontvangen..."
+        
+        Button:
+            text: "Terug naar hoofdmenu"
+            size_hint_y: 0.2
+            on_press: root.manager.current = 'menu'
+            
+""")
+
+
+
+
+class MenuScreen(Screen):
+    pass
+
+class ActionScreen(Screen):
+    def inloggen(self):
+        def update_tekst(*args):
+            self.af_te_drukken_tekst = (
+            "Netlogin openen...   [ OK ]", \
+            "Netlogin openen...   [ OK ]\nGegevens invoeren... [ OK ]", \
+            "Netlogin openen...   [ OK ]\nGegevens invoeren... [ OK ]\nGegevens opsturen... [ OK ]", \
+            "Netlogin openen...   [ OK ]\nGegevens invoeren... [ OK ]\nGegevens opsturen... [ OK ]\nDownload:            [ 82%]\nUpload:              [100%]"
+            )
+            
+            try:
+                self.ids["outputvak"].text = self.af_te_drukken_tekst[self.index]
+                self.index += 1
+            except IndexError:
+                ## indien index out of range (dus: einde bereikt): stop. Dat
+                ## gaat volgens de documentatie door een False te returnen
+                ## bij een methode die is aangeroepen door Clock.
+                return False
+            
+            
+        self.index = 0
+        
+        ## Gebruik Clock ipv time.sleep om tekst te updaten om de zoveel tijd
+        Clock.schedule_interval(update_tekst, 0.5)
+
+class SettingsScreen(Screen):
+    pass
+
+
+
+
+class TestApp(App):
+    def build(self):
+        # Create the screen manager
+        
+        self.menuscherm = MenuScreen(name="menu")
+        self.actiescherm = ActionScreen(name="actie")
+        self.instellingenscherm = SettingsScreen(name="settings")
+        
+        self.root = ScreenManager(transition=NoTransition())
+        
+        self.root.add_widget(self.menuscherm)
+        self.root.add_widget(self.actiescherm)
+        self.root.add_widget(self.instellingenscherm)
+        
+        return self.root
+    
+    def ga_naar_actiescherm_en_log_in(self):
+        self.root.current = "actie"
+        print self.root.current
+        self.actiescherm.inloggen()
+        
+        ## Huidig vraagstuk: Hoe zorgen we dat een drukknop in self.menuscherm
+        ## leidt tot het wisselen naar een self.actiescherm, én dan automatisch
+        ## inlogt?
+        
+
+if __name__ == '__main__':
+    TestApp().run()
