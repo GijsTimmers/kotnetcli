0c53e3074174ab7c53e1a84d0fff3779a70fa59f
GijsTimmers <timmers.gijs@gmail.com>
2015-03-12 14:49:53 +0100

    outputvak app

---
+++ app-development/browser.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+## Dependencies:    python-mechanize, python-keyring, curses
+## Author:          Gijs Timmers: https://github.com/GijsTimmers
+## Contributors:    Gijs Timmers: https://github.com/GijsTimmers
+##                  Jo Van Bulck: https://github.com/jovanbulck
+
+## Licence:         CC-BY-SA-4.0
+##                  https://creativecommons.org/licenses/by-sa/4.0/
+
+## This work is licensed under the Creative Commons
+## Attribution-ShareAlike 4.0 International License. To view a copy of
+## this license, visit https://creativecommons.org/licenses/by-sa/4.0/ or
+## send a letter to Creative Commons, PO Box 1866, Mountain View,
+## CA 94042, USA.
+
+import re                               ## Basislib voor reguliere expressies
+import time                             ## Voor timeout om venster te sluiten
+import urllib                           ## Diverse URL-manipulaties
+import urlparse                         ## Diverse URL-manipulaties
+import mechanize                        ## Emuleert een browser
+import socket                           ## Voor ophalen IP
+import sys                              ## Basislib
+import os                               ## Basislib
+
+from bs4 import BeautifulSoup, Comment  ## Om webinhoud proper te parsen.
+
+import logging
+logger = logging.getLogger(__name__)
+
+## the maximum waiting time in seconds for browser connections
+BROWSER_TIMEOUT_SEC = 1.5
+
+## the login url containing the institution choice
+NETLOGIN_URL = "https://netlogin.kuleuven.be/cgi-bin/wayf2.pl?inst=kuleuven&lang=nl&submit=Ga+verder+%2F+Continue"
+
+## login rc codes contained in the response html page
+RC_LOGIN_SUCCESS            = 100
+RC_LOGIN_INVALID_USERNAME = 201
+RC_LOGIN_INVALID_PASSWORD = 202
+RC_LOGIN_MAX_IP             = 206
+
+## custom exceptions
+class WrongCredentialsException(Exception):
+    pass
+
+##TODO hier het ip address in opslaan (~ hieronder de rccode)
+class MaxNumberIPException(Exception):
+    pass
+    
+class UnknownRCException(Exception):
+    def __init__(self, rccode, html):
+        self.rccode = rccode
+        self.html = html
+
+    def get_info(self):
+        return (self.rccode, self.html)
+
+## The class doing the actual browser emulation work. One can extend this
+## class to add specific behavior (e.g. communicating; err catching; etc).
+## KotnetBrowser() is like an API: it contains all possible Browser operations.
+## So, we don't create a LoginBrowser(), LogoutBrowser(), etc. Instead, the
+## proper Worker() is instantiated by kotnetcli.py, and this instance calls
+## only the Browser() methods that it needs.
+## Some 
+class KotnetBrowser():
+    ## Note: the browser itself doesn't save any credentials. These are kept in a
+    ## credentials object that is supplied when needed
+    def __init__(self):
+        self.browser = mechanize.Browser()
+        self.browser.addheaders = [('User-agent', 'Firefox')]
+    
+    ## returns True | False depending on whether or not the user seems to be on the
+    ## kotnet network (connect to  netlogin.kuleuven.be)
+    def bevestig_kotnetverbinding(self):
+        ## try to open a TCP connection on port 443 with a maximum waiting time
+        try:
+            sock = socket.create_connection(("netlogin.kuleuven.be", 443), BROWSER_TIMEOUT_SEC)
+            sock.close()
+            return True
+        except:
+            return False
+    
+    def login_open_netlogin(self):
+        response = self.browser.open(NETLOGIN_URL, \
+        timeout=BROWSER_TIMEOUT_SEC)
+        #html = response.read()
+
+    #def login_kies_kuleuven(self):
+    #    self.browser.select_form(nr=1)
+    #    self.browser.submit()
+    
+    def login_input_credentials(self, creds):
+        (gebruikersnaam, wachtwoord) = creds.getCreds()
+        self.browser.select_form(nr=1)
+        self.browser.form["uid"] = gebruikersnaam
+        wachtwoordvaknaam = \
+        self.browser.form.find_control(type="password").name
+        self.browser.form[wachtwoordvaknaam] = wachtwoord
+    
+    def login_send_credentials(self):
+        self.browser.submit()
+
+    ## This method parses the server's response. On success, it returns a tuple of
+    ## length 2: (downloadpercentage, uploadpercentage); else it raises an
+    ## appropriate exception
+    def login_parse_results(self):
+        html = self.browser.response().read()
+
+        soup = BeautifulSoup(html)
+
+        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
+        ## bevat de status.
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        p = re.compile("weblogin: rc=\d+")
+        for c in comments:
+            m = p.search(c)
+            #m = p.findall(c)
+            #print m
+            if m:
+                rccode = int(m.group().strip("weblogin: rc="))
+                ## m = p.search(c): zoekt naar de RE p in c. Indien die is
+                ## gevonden, is m een object; m.group() geeft vervolgens
+                ## weer waar de RE matcht. Dit doet het maar één keer; als
+                ## je strings op meerdere matches wilt controleren, moet je
+                ## p.findall(c) gebruiken, zoals hieronder.
+
+        logger.debug("rccode is %s", rccode)
+
+        if rccode == RC_LOGIN_SUCCESS:
+            ## downloadpercentage parsen
+            p = re.compile("\d+")
+            m = p.findall(comments[6])
+            downloadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
+
+            ## uploadpercentage parsen
+            p = re.compile("\d+")
+            m = p.findall(comments[7])
+            uploadpercentage = int(round(float(m[0]) / float(m[1]) * 100, 0))
+
+            return (downloadpercentage, uploadpercentage)
+
+        elif (rccode == RC_LOGIN_INVALID_USERNAME) or \
+            (rccode == RC_LOGIN_INVALID_PASSWORD):
+            raise WrongCredentialsException()
+            
+        elif rccode == RC_LOGIN_MAX_IP:
+            raise MaxNumberIPException()
+
+        else:
+            raise UnknownRCException(rccode, html)
+    
+    def logout_input_credentials(self):
+        ## Lokale IP ophalen: lelijk, maar werkt goed en is snel. Is waar-
+        ## schijnlijk de kortste code die crossplatform werkt, wat op zich
+        ## wel vreemd is. Meer informatie:        
+        ## http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com", 80))
+        self.uitteloggenip = s.getsockname()[0]
+        s.close()
+        
+        ## Logoutformulier aanmaken, dan lokatie omvormen tot file://-URL zodat
+        ## het browserobject hem kan gebruiken als lokatie.
+        gegevens = {"uitteloggenip" : self.uitteloggenip, 
+                    "gebruikersnaam": self.gebruikersnaam}
+        
+        with open("logoutformuliertemplate.html", "r") as logoutformulier_in:
+            data = logoutformulier_in.read()
+            data = data.format(**gegevens) ## gegevens worden hier ingevuld
+        with open("logoutformulieringevuld.html", "w") as logoutformulier_out:
+            logoutformulier_out.write(data)
+        
+        self.url_logoutformulier = urlparse.urljoin("file:", \
+        os.path.abspath("logoutformulieringevuld.html"))
+    
+    def logout_send_credentials(self):
+        self.browser.open(self.url_logoutformulier, timeout=1.8)
+        self.browser.select_form(nr=0)
+        self.browser.submit()
+        os.remove("logoutformulieringevuld.html")
+    
+    ## returns True if logout is succesful; False when it fails
+    def logout_parse_results(self):
+        html = self.browser.response().read()
+        soup = BeautifulSoup(html)
+
+        ## Zoek naar de rc-code in de comments van het html-bestand. Deze
+        ## bevat de status.
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        p = re.compile("weblogout: rc=\d+")
+
+        rccode = 100
+        ## if not error codes appear, assume that everything went OK.
+        for c in comments:
+            m = p.search(c)
+            #m = p.findall(c)
+            #print m
+            if m:
+                rccode = int(m.group().strip("weblogout: rc="))
+
+        if rccode == 100:
+            ## succesvolle logout
+            return True
+
+        elif rccode == 207:
+            ## al uitgelogd
+            print "U had uzelf reeds succesvol uitgelogd."
+            return False
+        else:
+            print html
+
+class DummyBrowser():
+    ## allow custom test behavior via params
+    def __init__(self, dummy_timeout, kotnet_online, netlogin_unavailable, rccode, downl, upl):
+        self.dummy_timeout = dummy_timeout
+        self.kotnet_online=kotnet_online
+        self.netlogin_unavailable = netlogin_unavailable
+        self.rccode = rccode
+        self.download = abs(downl) %101
+        self.upload = abs(upl) %101
+    
+    def bevestig_kotnetverbinding(self):
+        return self.kotnet_online
+
+    def login_open_netlogin(self):
+        if (not self.netlogin_unavailable):
+            time.sleep(self.dummy_timeout)
+        else:
+            raise Exception
+
+    #def login_kies_kuleuven(self):
+    #    time.sleep(0.1)
+    
+    def login_input_credentials(self, creds):
+        time.sleep(self.dummy_timeout)
+
+    def login_send_credentials(self):
+        time.sleep(self.dummy_timeout)
+
+    def login_parse_results(self):
+        logger.debug("rccode is %s", self.rccode)
+        
+        if self.rccode == RC_LOGIN_SUCCESS:
+            return (self.download, self.upload)
+
+        elif (self.rccode == RC_LOGIN_INVALID_PASSWORD) or \
+            (self.rccode == RC_LOGIN_INVALID_USERNAME):
+            raise WrongCredentialsException()
+            
+        elif self.rccode == RC_LOGIN_MAX_IP:
+            raise MaxNumberIPException()
+
+        else:
+            raise UnknownRCException(self.rccode, "\n<html>\n<p>the dummy html page</p>\n</html>\n")
+
+
+'''    
+## Deze klasse bevat functies voor de forceer-loginmethode. Gaan we nu nog
+## niet gebruiken. Deze klasse wordt later waarschijnlijk samengevoegd met
+## KotnetBrowser().
+class KotnetForceerBrowser(KotnetBrowser):
+    def __init__():
+        pass
+    
+    def uitteloggenipophalen(self):
+        html = self.browser.response().read()
+        soup = BeautifulSoup(html)
+
+        forms = soup.findAll("form")
+        form = forms[1]
+        uitteloggenip = form.contents[3]["value"]
+
+        return uitteloggenip    
+'''
--- app-development/juiste_verbindingen.py
+++ app-development/juiste_verbindingen.py
@@ -17,18 +17,141 @@
 
 import re
 import sys
-
-
+import threading
+from browser import KotnetBrowser
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.clock import Clock ## Om een interval in te bouwen, zonder de loop te blokkeren
 from kivy.storage.jsonstore import JsonStore
+
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from pygments.lexers import TextLexer
 
 class MenuScreen(Screen):
     pass
 
 class ActionScreen(Screen):
+    def print_generic_balk(self, percentage, style, color, stop_color, stop_style):        
+            percentagefloat = float(percentage)
+            percentagestring = str(percentage)
+            
+            lengteVanBalkfloat = 15.0
+            lengteVanBalkint = 15
+            lengteVanRuimteVoorPercentages = 3
+            
+            aantalStreepjesObvPercentage = int(round(percentagefloat/100.0 * lengteVanBalkfloat))
+            return style + "[" + color + \
+            "=" * aantalStreepjesObvPercentage + stop_color + \
+            " " * (lengteVanBalkint-aantalStreepjesObvPercentage) + \
+            "][" + \
+            " " * (lengteVanRuimteVoorPercentages - len(percentagestring)) + \
+            color + percentagestring + "%" + \
+            stop_color + "]" + stop_style
+        
+    def print_balk(self, percentage):
+            return self.print_generic_balk(percentage, "", "", "", "")
+    
+    def worker1(self):
+        self.status = "bezig"
+        self.kb = KotnetBrowser()
+        self.kb.login_open_netlogin()
+        self.ids["outputvak"].text = "Netlogin openen.......... [ OK ]\n"
+        self.status = "klaar"
+    
+    def worker2(self):
+        class creds():
+            def getCreds(self):
+                keyring = JsonStore("keyring.json")
+                gebruikersnaam = keyring.get("kotnetcli")["gebruikersnaam"]
+                wachtwoord     = keyring.get("kotnetcli")["wachtwoord"]
+                
+                print "----------------"
+                print "Gebruikersnaam: " + gebruikersnaam
+                print "Wachtwoord:     " + wachtwoord
+                print "----------------"
+                print "--> Opgehaald."
+                
+                return (gebruikersnaam, wachtwoord)
+            
+        self.kb.login_input_credentials(creds())
+        self.ids["outputvak"].text += "Gegevens invoeren........ [ OK ]\n"
+    
+    def worker3(self):
+        self.kb.login_send_credentials()
+        self.ids["outputvak"].text += "Gegevens opsturen........ [ OK ]\n"
+        
+        ## Parsen gaat wél op PC, niet op Android: bs4 conflicteert met mechanize
+        ## rond een bepaalde library, die ze allebei nodig hebben.
+        ## Workaround: parsen met iets anders dan bs4.
+        
+        #(up, down) = self.kb.login_parse_results()
+        
+        #self.ids["outputvak"].text += "Download:            [" + str(down) + "%]\n"
+        #self.ids["outputvak"].text += "Upload:              [" + str(up)   + "%]\n"
+        #self.ids["outputvak"].text += "Download " + self.print_balk(down) + "\n"
+        #self.ids["outputvak"].text += "Upload   " + self.print_balk(up)   + "\n"
+    
+    def inloggen_go(self, dt):
+        self.status = "klaar"
+        thread = threading.Thread(target=self.worker1)
+        thread.start()
+        thread.join()
+        thread = threading.Thread(target=self.worker2)
+        thread.start()
+        thread.join()
+        thread = threading.Thread(target=self.worker3)
+        thread.start()
+        
+        #Clock.schedule_once(self.worker1)
+        #Clock.schedule_once(self.worker2)
+        #Clock.schedule_once(self.worker3)
+        #event = Clock.create_trigger(self.worker1)
+        #event()
+        #event = Clock.create_trigger(self.worker2)
+        #event()
+        #event = Clock.create_trigger(self.worker3)
+        #event()
+        """
+        self.ids["outputvak"].lexer = TextLexer()
+                
+        self.kb = KotnetBrowser()
+        self.kb.login_open_netlogin()
+        self.ids["outputvak"].text = "Netlogin openen.......... [ OK ]\n"
+    
+    
+        class creds():
+            def getCreds(self):
+                keyring = JsonStore("keyring.json")
+                gebruikersnaam = keyring.get("kotnetcli")["gebruikersnaam"]
+                wachtwoord     = keyring.get("kotnetcli")["wachtwoord"]
+                
+                print "----------------"
+                print "Gebruikersnaam: " + gebruikersnaam
+                print "Wachtwoord:     " + wachtwoord
+                print "----------------"
+                print "--> Opgehaald."
+                
+                return (gebruikersnaam, wachtwoord)
+            
+        self.kb.login_input_credentials(creds())
+        self.ids["outputvak"].text += "Gegevens invoeren........ [ OK ]\n"
+    
+    
+        self.kb.login_send_credentials()
+        self.ids["outputvak"].text += "Gegevens opsturen........ [ OK ]\n"
+        
+        (up, down) = self.kb.login_parse_results()
+        
+        #self.ids["outputvak"].text += "Download:            [" + str(down) + "%]\n"
+        #self.ids["outputvak"].text += "Upload:              [" + str(up)   + "%]\n"
+        self.ids["outputvak"].text += "Download " + self.print_balk(down) + "\n"
+        self.ids["outputvak"].text += "Upload   " + self.print_balk(up)   + "\n"
+        """
+    
+    def inloggen(self):
+        Clock.schedule_once(self.inloggen_go, 0)
+        
+    """
     def inloggen(self):
         def update_tekst(*args):
             self.af_te_drukken_tekst = (
@@ -52,32 +175,134 @@ class ActionScreen(Screen):
         
         ## Gebruik Clock ipv time.sleep om tekst te updaten om de zoveel tijd
         Clock.schedule_interval(update_tekst, 0.5)
+    """
+    def uitloggen(self):
+        print "Ik wil uitloggen."
 
 class SettingsScreen(Screen):
-    pass
+    def saveOrForget(self):
+        ## Bepaalt adhv de toestand van de opslaanvergeetknop of er opgeslagen
+        ## dan wel vergeten moet worden.
+        if self.ids["opslaanvergeetknop"].toestand == "opslaan":
+            self.saveCredentials()
+        elif self.ids["opslaanvergeetknop"].toestand == "vergeten":
+            self.forgetCredentials()
+    
+    def saveCredentials(self):
+        ## Leest de gegevens uit uit de velden. Indien er een foute gebruikers-
+        ## naam is (bvb 0123456), of een leeg wachtwoord, krijgt de gebruiker
+        ## een melding door verandering van de tekst op de knop. Indien het
+        ## wel lijkt te kloppen, worden de gegevens opgeslagen in keyring.json.
+        
+        gebruikersnaam = self.ids["veld_gebruikersnaam"].text
+        wachtwoord     = self.ids["veld_wachtwoord"].text
+        
+        print "----------------"
+        print "Gebruikersnaam: " + gebruikersnaam
+        print "Wachtwoord:     " + wachtwoord
+        print "----------------"
+        
+        if (not re.match("[a-z][0-9]+", gebruikersnaam)) or (wachtwoord == ""):
+            ## Als de gebruikersnaam geen geldig s-/r-nummer is, of het wachtwoord
+            ## is leeg, dan wordt er niets opgeslagen en geven we een melding
+            ## via de knop.
+            print "--> Gegevens kloppen niet, controleer."
+            self.ids["opslaanvergeetknop"].text = "Controleer uw gegevens en klik opnieuw."
+        else:
+            ## Als alles klopt, slaan we het op in de keyring.
+            keyring = JsonStore("keyring.json")
+            keyring.put("kotnetcli", \
+                        gebruikersnaam = gebruikersnaam, \
+                        wachtwoord     = wachtwoord)
+            print "--> Opgeslagen in keyring.json."
+            
+            self.ids["opslaanvergeetknop"].toestand = "vergeten"
+            self.ids["opslaanvergeetknop"].text = "Gebruikersnaam en wachtwoord opnieuw instellen"
+            self.ids["opslaanvergeetknop"].background_color = [2.5, 1, 1, 1]
+            self.ids["veld_gebruikersnaam"].disabled = True
+            self.ids["veld_wachtwoord"].disabled = True
+            self.ids["veld_gebruikersnaam"].text = gebruikersnaam
+            self.ids["veld_wachtwoord"].text = wachtwoord
+            self.ids["veld_gebruikersnaam"].font_size = 90
+            
+    
+    def forgetCredentials(self):
+        keyring = JsonStore("keyring.json")
+        keyring.delete("kotnetcli")
+        print "----------------"
+        print "Gebruikersnaam: " + "????????"
+        print "Wachtwoord:     " + "????????"
+        print "----------------"
+        print "--> Verwijderd uit keyring.json."
+        self.ids["opslaanvergeetknop"].toestand = "opslaan"
+        self.ids["opslaanvergeetknop"].text = "Opslaan"
+        self.ids["opslaanvergeetknop"].background_color = [1, 1, 1, 1]
+        self.ids["veld_gebruikersnaam"].disabled = False
+        self.ids["veld_wachtwoord"].disabled = False
+        self.ids["veld_gebruikersnaam"].text = ""
+        self.ids["veld_wachtwoord"].text = ""
+        self.ids["veld_gebruikersnaam"].font_size = 90
 
 
 class KotnetApp(App):
     def build(self):
-        ## Screen manager aanmaken
+        ## Kivy-instellingen uitschakelen bij druk op Menu-knop
+        self.use_kivy_settings = False
         
+        ## Screen manager aanmaken
         self.menuscherm = MenuScreen(name="menu")
         self.actiescherm = ActionScreen(name="actie")
         self.instellingenscherm = SettingsScreen(name="settings")
         
         self.root = ScreenManager(transition=NoTransition())
-        
         self.root.add_widget(self.menuscherm)
         self.root.add_widget(self.actiescherm)
         self.root.add_widget(self.instellingenscherm)
         
+        self.check_of_credentials_al_bestaan()
+        
         return self.root
     
+    def check_of_credentials_al_bestaan(self):
+        keyring = JsonStore("keyring.json")
+        
+        try:
+            ## Ze bestaan wel
+            gebruikersnaam = keyring.get("kotnetcli")["gebruikersnaam"]
+            wachtwoord     = keyring.get("kotnetcli")["wachtwoord"]
+            
+            print "----------------"
+            print "Gebruikersnaam: " + gebruikersnaam
+            print "Wachtwoord:     " + wachtwoord
+            print "----------------"
+            print "--> Opgehaald."
+            
+            
+            self.instellingenscherm.ids["opslaanvergeetknop"].toestand = "vergeten"
+            self.instellingenscherm.ids["opslaanvergeetknop"].text = "Gebruikersnaam en wachtwoord opnieuw instellen"
+            self.instellingenscherm.ids["opslaanvergeetknop"].background_color = [2.5, 1, 1, 1]
+            self.instellingenscherm.ids["veld_gebruikersnaam"].disabled = True
+            self.instellingenscherm.ids["veld_wachtwoord"].disabled = True
+            self.instellingenscherm.ids["veld_gebruikersnaam"].text = gebruikersnaam
+            self.instellingenscherm.ids["veld_wachtwoord"].text = wachtwoord
+            self.instellingenscherm.ids["veld_gebruikersnaam"].font_size = 120
+            
+        except KeyError:
+            ## Ze bestaan nog niet
+            print "Gebruikersnaam en wachtwoord zijn onbekend."
+            #self.menuscherm.ids["inlogknop"].disabled = True
+            #self.menuscherm.ids["uitlogknop"].disabled = True
+        
     def ga_naar_actiescherm_en_log_in(self):
         self.root.current = "actie"
-        print self.root.current
         self.actiescherm.inloggen()
     
+    def ga_naar_actiescherm_en_log_uit(self):
+        self.root.current = "actie"
+        self.actiescherm.uitloggen()
+    
+    
+    """
     def on_pause(self):
         ## Here you can save data if needed
         return True
@@ -85,7 +310,7 @@ class KotnetApp(App):
     def on_resume(self):
         ## Here you can check if any data needs replacing (usually nothing)
         pass
-        
+    """
         
 
 if __name__ == '__main__':
